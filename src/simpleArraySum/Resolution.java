047206a0ba7f8dc2c4d244eb4f8605be98226731
Kleber Vales <ksvales@outlook.pt>
2024-12-30 09:16:26 -0300

    Resolution of the exercise

--- src/simpleArraySum/Resolution.java
+++ src/simpleArraySum/Resolution.java
@@ -1,10 +1,41 @@
 package simpleArraySum;
 
-public class Resolution {
+import static java.util.stream.Collectors.toList;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+class Result {
+
+	public static int simpleArraySum(List<Integer> ar) {
+
+		return ar.stream().mapToInt(Integer::intValue).sum();
 
 	}
 
+}
+
+public class Resolution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+		int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+		List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+				.map(Integer::parseInt).collect(toList());
+
+		int result = Result.simpleArraySum(ar);
+
+		bufferedWriter.write(String.valueOf(result));
+		bufferedWriter.newLine();
+
+		bufferedReader.close();
+		bufferedWriter.close();
+	}
 }
